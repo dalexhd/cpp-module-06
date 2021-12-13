3b145e466d8cde810ba2d878becf04710d8dcecd
DalexHD <eldalexhd@gmail.com>
2021-12-13 14:08:30 +0000

    feat: add ex01.

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,17 +6,43 @@
 /*   By: aborboll <aborboll@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/13 13:56:47 by aborboll          #+#    #+#             */
-/*   Updated: 2021/12/13 13:57:32 by aborboll         ###   ########.fr       */
+/*   Updated: 2021/12/13 14:08:16 by aborboll         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-/*
-** Incluimos las librerias esenciales.
-*/
 #include <iostream>
 #include <string.h>
 
-int main(void)
+struct Data
 {
+	std::string s1;
+	std::string s2;
+};
+
+
+uintptr_t	serialize(Data* ptr)
+{
+	return(reinterpret_cast<uintptr_t>(ptr));
+}
+
+Data*	deserialize(uintptr_t raw)
+{
+	return(reinterpret_cast<Data *>(raw));
+}
+
+int	main(void)
+{
+	Data	salut;
+
+	salut.s1 = "Hola";
+	salut.s2 = "Mundo";
+
+	uintptr_t	serialized_ptr = serialize(&salut);
+
+	std::cout << "Salut (serialized): " << serialized_ptr << std::endl;
+
+	Data	*ptr = deserialize(serialized_ptr);
+
+	std::cout << "Salut (deserialized): " << ptr->s1 << " " << ptr->s2 << std::endl;
 	return (0);
 }
